2c55c9cfde854417ad39de74674d8e76b2710ed1
Tanay Pratap <tanay.mit@gmail.com>
2020-07-17 19:09:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>hooks workshop</h1>
+      <h2>welcome to tanaypratap's react hook workshop</h2>
+      <h3> don't forget to subscibe </h3>
+    </div>
+  );
+}
